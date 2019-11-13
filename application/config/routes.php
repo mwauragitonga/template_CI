5d71e608bb44cc3925b27bb1274515d47ae3444c
Cyrus-Muchiri <cyrusmuchirichomba@gmail.com>
2019-11-13 11:51:09 +0300

    Initail commit for code igniter template

---
+++ application/config/routes.php
@@ -0,0 +1,88 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+| -------------------------------------------------------------------------
+| URI ROUTING
+| -------------------------------------------------------------------------
+| This file lets you re-map URI requests to specific controller functions.
+|
+| Typically there is a one-to-one relationship between a URL string
+| and its corresponding controller class/method. The segments in a
+| URL normally follow this pattern:
+|
+|	example.com/class/method/id/
+|
+| In some instances, however, you may want to remap this relationship
+| so that a different class/function is called than the one
+| corresponding to the URL.
+|
+| Please see the user guide for complete details:
+|
+|	https://codeigniter.com/user_guide/general/routing.html
+|
+| -------------------------------------------------------------------------
+| RESERVED ROUTES
+| -------------------------------------------------------------------------
+|
+| There are three reserved routes:
+|
+|	$route['default_controller'] = 'welcome';
+|
+| This route indicates which controller class should be loaded if the
+| URI contains no data. In the above example, the "welcome" class
+| would be loaded.
+|
+|	$route['404_override'] = 'errors/page_missing';
+|
+| This route will tell the Router which controller/method to use if those
+| provided in the URL cannot be matched to a valid route.
+|
+|	$route['translate_uri_dashes'] = FALSE;
+|
+| This is not exactly a route, but allows you to automatically route
+| controller and method names that contain dashes. '-' isn't a valid
+| class or method name character, so it requires translation.
+| When you set this option to TRUE, it will replace ALL dashes in the
+| controller and method URI segments.
+|
+| Examples:	my-controller/index	-> my_controller/index
+|		my-controller/my-method	-> my_controller/my_method
+*/
+$route['default_controller'] = 'Analytics_controller';
+$route['general'] = 'Analytics_controller/index';
+$route['payments'] = 'Analytics_controller/payments';
+$route['web'] ='Web_controller/webAnalytics';
+$route['videoViewers'] ='Web_controller/videoViewers';
+$route['bookReaders'] ='Web_controller/bookReaders';
+$route['attempted_payments'] ='Web_controller/payments';
+$route['app'] = 'AppAnalytics/index';
+$route['evaluations'] = 'Evaluations_controller/evaluations';
+$route['examsAttemptsToday'] = 'Evaluations_controller/examsAttemptsToday';
+$route['examAttempts'] = 'Evaluations_controller/examAttempts';
+
+$route['tiles'] = 'Payments/tiles';//api
+$route['graphs'] ='Payments/graphs';//api
+$route['authentication'] = 'Analytics_controller/authentication';
+$route['login'] = 'Analytics_controller/login';
+$route['logout'] = 'Analytics_controller/logout';
+$route['accounts'] = 'Analytics_controller/accounts';
+$route['accountsByDay'] = 'Analytics_controller/accountsByDay';
+$route['filter'] = 'Analytics_controller/filterSignups';
+$route['weeklyUsers'] = 'Analytics_controller/weeklyActiveUsers';
+$route['monthlyUsers'] = 'Analytics_controller/monthlyActiveUsers';
+$route['accountsview'] = "Analytics_controller/accountsByDay";
+$route['accountsByDay'] = "Analytics_controller/signUps_by_Day";//ajax
+$route['videos'] = "AppAnalytics/videos";
+$route['ebooks'] = "AppAnalytics/ebooks";
+$route['payers'] = "Analytics_controller/payers";
+$route['users/(:num)'] = "AppAnalytics/users/$1";
+$route['signins'] ="AppAnalytics/signins";
+$route['schools']= "Schools/usage";
+$route['schools/(:any)'] = "Schools/students/$1";
+$route['unique']= "AppAnalytics/duplicates";
+$route['reg_schools'] = "Schools/reg_schools";
+$route['top_reading'] = "Schools/top_reading";
+$route['schools/users/(:any)'] = "Schools/users/$1";
+$route['404_override'] = '';
+$route['translate_uri_dashes'] = FALSE;
